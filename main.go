c519a823141c1bc4557327284d791cc72b4be516
Alicja <k2l8m11n2@gmail.com>
2020-02-15 00:57:14 +0100

    Add a version check

--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
+	"log"
+
 	"github.com/dgraph-io/badger"
 )
 
 func main() {
-	// Open the Badger database located in the /tmp/badger directory.
-	// It will be created if it doesn't exist.
-	db, err := badger.Open(badger.DefaultOptions("/tmp/badger"))
+	version := []byte{0}
+
+	db, err := badger.Open(badger.DefaultOptions(".snappy-chandler"))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer db.Close()
-	// Your code here…
+	err = db.Update(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("version"))
+		if err != nil {
+			log.Println("version key not found, assuming fresh db and writing our own version number")
+			txn.Set([]byte("version"), []byte{0})
+			return nil
+		}
+		return item.Value(func(val []byte) error {
+			if err != nil {
+				return fmt.Errorf("version check failed: %w", err)
+			}
+			if val[0] != version[0] { // yep, I hope we never get past 256 versions
+				return fmt.Errorf("version check failed: %v (db) != %v (tool)", val, version)
+			}
+			return nil
+		})
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
