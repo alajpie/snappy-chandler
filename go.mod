e29a582a8047a2d24ca80953e95f788a1e8e444e
Alicja <k2l8m11n2@gmail.com>
2020-02-15 02:12:47 +0100

    Implement blob insertion (untested)

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/dgraph-io/badger v1.6.0
 	github.com/restic/chunker v0.3.0
+	lukechampine.com/blake3 v1.0.0
 )
--- main.go
+++ main.go
@@ -3,20 +3,25 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/dgraph-io/badger"
-	"github.com/lukechampine/blake3"
 	"github.com/restic/chunker"
+	"lukechampine.com/blake3"
 )
+
+func fatalize(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	version := []byte{0}
 
 	db, err := badger.Open(badger.DefaultOptions(".snappy-chandler"))
-	if err != nil {
-		log.Fatal(err)
-	}
+	fatalize(err)
 	defer db.Close()
 	err = db.Update(func(txn *badger.Txn) error {
 		item, err := txn.Get([]byte("version"))
@@ -42,7 +47,67 @@ func main() {
 			return nil
 		})
 	})
+	fatalize(err)
+}
+
+func insert(db *badger.DB, r io.Reader) ([32]byte, error) {
+	var poly chunker.Pol
+	err := db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("polynomial"))
+		if err != nil {
+			return err
+		}
+		item.Value(func(val []byte) error {
+			poly = chunker.Pol(binary.LittleEndian.Uint64(val))
+			return nil
+		})
+		return nil
+	})
 	if err != nil {
-		log.Fatal(err)
+		return [32]byte{}, err
 	}
+	ch := chunker.New(r, poly)
+	buf := make([]byte, 16*1024*1024)
+	var hashes []byte
+	var superhash [32]byte
+	err = db.Update(func(txn *badger.Txn) error {
+		for {
+			chunk, err := ch.Next(buf)
+			if err == io.EOF {
+				return nil
+			}
+			if err != nil {
+				return err
+			}
+
+			data := make([]byte, len(chunk.Data))
+			// BadgerDB can't have data changed under it before it commits
+			// chunker reuses the underlying buffer so we have to copy it off
+			// TODO: commit after every chunk for memory savings?
+			copy(data, chunk.Data)
+			hash := blake3.Sum256(chunk.Data)
+
+			_, err = txn.Get(append([]byte("chunk/"), hash[:]...))
+			if err == nil { // chunk exists
+				hashes = append(hashes, hash[:]...)
+				continue
+			}
+			if err != badger.ErrKeyNotFound {
+				return err
+			}
+
+			// chunk doesn't exist, inserting
+			err = txn.Set(append([]byte("chunk/"), hash[:]...), data)
+			if err != nil {
+				return err
+			}
+		}
+		superhash = blake3.Sum256(hashes)
+		return txn.Set(append([]byte("blob/"), superhash[:]...), hashes)
+	})
+	if err != nil {
+		return [32]byte{}, err
+	}
+
+	return superhash, nil
 }
